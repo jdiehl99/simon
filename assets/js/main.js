b18b31c17e7cb8d2255566274f5c1fa1f6d302c1
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2018-12-16 16:09:40 -0500

    chris-branch-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,19 +16,41 @@ function newGame() {
     clearGame();
 }
 
-
+console.log(computerGenerateChoice());
 
 // function to start clicks of computer and saves to computer array
 function computerGenerateChoice() {
-  
+
     // Math.floor(Math.random() * 4) ;
-    game.computerArrayOfChoicesToMatch.push(game.colors[(Math.floor(Math.random() * 4))])
+    simonSays.computerArrayOfChoicesToMatch.push(simonSays.colors[(Math.floor(Math.random() * 4))])
+    displayComputerMove();
+
+   // console.log(simonSays.computerArrayOfChoicesToMatch)
+}
+
+
+function displayComputerMove() {
+    // set imterveral
+    const computerChoice = simonSays.computerArrayOfChoicesToMatch[0];
+    console.log(`=>>> #${computerChoice}`)
+    var div = $(computerChoice)
+    console.log(div)
+    div.css("opacity", 1)
 
 
 }
 
 
+// $("img").on("click", function () {
+//     console.log("Img was clicked");
+//     const playerChoice = $(this).attr("id");
+//     console.log(playerChoice);
+//     playerArrayOfChoicestoMatch.push(playerChoice);
+//     $("img").css("opacity", 0.4);
+//     $(this).css("opacity", 1);
 
+//     console.log(playerArrayOfChoicestoMatch);
+// });
 
 // function that empties the arrays 
 function clearGame() {
